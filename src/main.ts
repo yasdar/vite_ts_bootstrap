da53f8c4260d19c87acfe7ca0e24268058b87c95
yasdar <dardour.yassine@gmail.com>
2025-11-24 09:23:19 +0100

    empty vite ts bootstrap project 2025

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import "./style.css";
+// Import Bootstrap's CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Import Bootstrap's JavaScript bundle (includes Popper.js)
+/*import * as bootstrap from "bootstrap";
+
+import typescriptLogo from "./typescript.svg";
+import viteLogo from "/vite.svg";*/
+
+//<img src="${viteLogo}" class="logo" alt="Vite logo" />
